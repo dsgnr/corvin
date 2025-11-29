4f016dca8c3b6a862010503f26e76c360b21ae80
dsgnr <email@danielhand.io>
2025-11-29 22:03:49 +0000

    refactor: export all models from package

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,1 +1,12 @@
-# Models package
+from app.models.profile import Profile
+from app.models.video_list import VideoList
+from app.models.video import Video
+from app.models.history import History, HistoryAction
+
+__all__ = [
+    "Profile",
+    "VideoList",
+    "Video",
+    "History",
+    "HistoryAction",
+]
